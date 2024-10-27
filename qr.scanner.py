ae3af59800641ceb0d30b236ba5ee260cbdd7fe7
rahulrahu1 <151120973+rahulrahu1@users.noreply.github.com>
2024-10-27 19:08:33 +0530

    Update qr.scanner.py

--- qr.scanner.py
+++ qr.scanner.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import numpy as np
 
-zbar_library_path = "C:\\Users\\rahul\\AppData\\Roaming\\Python\\Python38\\site-packages\\pyzbar\\__init__.py"
+zbar_library_path = "C:\\Users\\AppData\\Roaming\\Python\\Python38\\site-packages\\pyzbar\\__init__.py"
 os.environ["DYLD_FALLBACK_LIBRARY_PATH"] = "C:\\path\\to\\libiconv.dll;C:\\path\\to\\libzbar-64.dll"
 
 from pyzbar.pyzbar import decode
